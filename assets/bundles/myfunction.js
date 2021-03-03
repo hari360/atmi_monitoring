ae6f40a85d7e98f0b4d98aab81c84a311827eaa9
Hari Pramono <hari@alto.id>
2021-03-03 12:06:51 +0700

    add detail term  dashboard

--- assets/bundles/myfunction.js
+++ assets/bundles/myfunction.js
@@ -144,6 +144,7 @@ $(document).ready(function(){
     paging: true, 
     info: true, 
     searching: true,
+    // scrollY:        "350px",
   });
 
   var table_card_retain = $('#dt_card_retain_cardbase').DataTable({
@@ -155,9 +156,22 @@ $(document).ready(function(){
       { width: '20%', targets: 0 }
     ],
     fixedColumns: true,
+    // scrollY:        "350px",
   });
 
-  var table_card_retain = $('#dt_offline').DataTable({
+  var table_count_trx_atm = $('#dt_count_trx_cardbase').DataTable({
+    iDisplayLength:20,
+    paging: true, 
+    info: true, 
+    searching: true,
+    columnDefs: [
+      { width: '20%', targets: 0 }
+    ],
+    fixedColumns: true,
+    // scrollY:        "350px",
+  });
+
+  var table_offline = $('#dt_offline').DataTable({
     iDisplayLength:100,
     paging: true, 
     info: true, 
@@ -190,7 +204,29 @@ $(document).ready(function(){
     fixedColumns: true,
   });
 
-  var table_inservice = $('#dt_term_idle').DataTable({
+  var table_inservice = $('#dt_faulty').DataTable({
+    iDisplayLength:100,
+    paging: true, 
+    info: true, 
+    searching: true,
+    columnDefs: [
+      { width: '20%', targets: 0 }
+    ],
+    fixedColumns: true,
+  });
+
+  var table_inservice = $('#dt_idle_term').DataTable({
+    iDisplayLength:100,
+    paging: true, 
+    info: true, 
+    searching: true,
+    columnDefs: [
+      { width: '20%', targets: 0 }
+    ],
+    fixedColumns: true,
+  });
+
+  var table_inservice = $('#dt_saldo_min').DataTable({
     iDisplayLength:100,
     paging: true, 
     info: true, 
@@ -215,6 +251,7 @@ $(document).ready(function(){
       paging: true, 
       info: true, 
       searching: true,
+      // scrollY:        "350px",
   });
 
   $('#dt_terminal_log tbody').on('click', 'td.details-control', function () {
